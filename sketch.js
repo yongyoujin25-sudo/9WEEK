632bc3b845000171a9dc6aca7ffd3e2526c0e8fc
yongyoujin25-sudo <yongyoujin25@gmail.com>
2025-11-04 12:06:54 +0900

    알까기

--- sketch.js
+++ sketch.js
@@ -10,14 +10,19 @@ function setup() {
 function draw() {
   background(220);
 
-  let gravity = createVector(0, 0.3);
-  ball.addForce(gravity);
+//   let gravity = createVector(0, 0.3);
+//   ball.addForce(gravity);
 
-  if(mouseIsPressed) {
-    let wind = createVector(0.1,0);
-    ball.addForce(wind);
-  } 
 
   ball.update();
   ball.show();
+}
+
+function mouseReleased() {
+    let d = dist(ball.pos.x, ball.pos.y, mouseX, mouseY);
+    if (d < ball.w/2){
+      let ff = p5.Vector.sub(ball.pos, createVector(mouseX,mouseY));
+      ff.mult(0.3);
+      ball.addForce(ff);
+    }
 }
